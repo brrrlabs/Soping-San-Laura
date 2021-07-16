0c7439887fe2f0f0bf77239955b4e73524f93710
aripnk <anurkhoirudin@gmail.com>
2021-07-17 06:08:28 +0700

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,53 @@ $('#becoming-path').hover(function() {
 }, function() {
   $('#becoming-path').attr("fill", "#9EFF0000");
 });
+
+$('#mursida-path').hover(function() {
+  $('#mursida-path').attr("fill", "#9EFF00");
+}, function() {
+  $('#mursida-path').attr("fill", "#9EFF0000");
+});
+
+$('#mursida-path').click(function() {
+  // $('#mursida-video')[0].play();
+  var video = $("#mursida-video").get(0);
+  if ( video.paused ) {
+    video.play();
+    $("#mursida-path").hide(500);
+  }
+});
+
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  let myForm = document.getElementById('pizzaOrder');
+  let formData = new FormData(myForm)
+  fetch('/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  }).then(() => {
+    $("form").hide(500);
+    $("#success-form").show(500);
+  }).catch((error) =>
+    alert(error))
+}
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+// $("#question-form").submit(function(event) {
+//   event.preventDefault();
+//   $.ajax({
+//     url: 'https://api.mailslurp.com/forms?_to=brrrlabs+sopingsanlaura@gmail.com', // url where to submit the request
+//     type: "POST", // type of action POST || GET
+//     dataType: 'json', // data type
+//     data: $("#question-form").serialize(), // post data || get data
+//     success: function(result) {
+//       // you can see the result fr Form submission from your site. om the console
+//       // tab of the developer tools
+//       alert(JSON.parse(result));
+//     },
+//     error: function(xhr, resp, text) {
+//       alert(xhr, resp, text);
+//     }
+//   })
+// });

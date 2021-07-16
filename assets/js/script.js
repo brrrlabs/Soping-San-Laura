0eb5f10dbaa5789ec67f5bacc5597653554e208b
aripnk <anurkhoirudin@gmail.com>
2021-07-16 12:46:34 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,21 @@ $('.title-wrapper').hover(function() {
   $('.title').css("text-decoration", "none");
 });
 
-$('#door-to-door-line').hover(function() {
+$('#door-to-door-path').hover(function() {
   $('#door-to-door-path').attr("fill", "#9EFF00");
 }, function() {
   $('#door-to-door-path').attr("fill", "#9EFF0000");
+});
+
+
+$('#rhizoma-path').hover(function() {
+  $('#rhizoma-path').attr("fill", "#9EFF00");
+}, function() {
+  $('#rhizoma-path').attr("fill", "#9EFF0000");
+});
+
+$('#becoming-path').hover(function() {
+  $('#becoming-path').attr("fill", "#9EFF00");
+}, function() {
+  $('#becoming-path').attr("fill", "#9EFF0000");
 });
